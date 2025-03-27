d88db44920b0a0fe599c1ccab9b100321ea19d5b
Potnuru.Sai Naidu <144212875+sainaidu401@users.noreply.github.com>
2025-03-27 15:16:37 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 
 describe('Integration Test: Nginx Web Server', function () {
   it('should return status 200 and the greeting from the HTML file', function (done) {
-    request('http://localhost:8080') // Ensure this URL matches your running container
+    request('http://localhost:8080') // Ensure this URL matches your running container new
       .get('/')
       .expect(200)
       .end((err, res) => {
